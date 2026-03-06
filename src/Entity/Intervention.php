cdba9b0f3f7c43742191e728dcbb84ee5f432da6
clem-clem06 <clement.julien@iia-formation.fr>
2026-03-06 12:25:47 +0100

    Add Intervention entity, repository, and migration

---
+++ src/Entity/Intervention.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InterventionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InterventionRepository::class)]
+class Intervention
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?\DateTime $dateDebut = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $dateFin = null;
+
+    /**
+     * @var Collection<int, Unite>
+     */
+    #[ORM\ManyToMany(targetEntity: Unite::class, inversedBy: 'interventions')]
+    private Collection $unites;
+
+    public function __construct()
+    {
+        $this->unites = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDateDebut(): ?\DateTime
+    {
+        return $this->dateDebut;
+    }
+
+    public function setDateDebut(\DateTime $dateDebut): static
+    {
+        $this->dateDebut = $dateDebut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTime
+    {
+        return $this->dateFin;
+    }
+
+    public function setDateFin(?\DateTime $dateFin): static
+    {
+        $this->dateFin = $dateFin;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Unite>
+     */
+    public function getUnites(): Collection
+    {
+        return $this->unites;
+    }
+
+    public function addUnite(Unite $unite): static
+    {
+        if (!$this->unites->contains($unite)) {
+            $this->unites->add($unite);
+        }
+
+        return $this;
+    }
+
+    public function removeUnite(Unite $unite): static
+    {
+        $this->unites->removeElement($unite);
+
+        return $this;
+    }
+}
